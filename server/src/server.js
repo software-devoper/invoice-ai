5edbea3ae793788b20b59dd5323441cf1cff5ca7
Subhadip Mondal <subhadipmondal0101@gmail.com>
2026-03-02 16:09:45 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -9,10 +9,12 @@ const PORT = Number(process.env.PORT || 5000);
 const startServer = async () => {
   await connectDB();
   app.listen(PORT, () => {
+    const emailProvider = String(process.env.EMAIL_PROVIDER || (process.env.RESEND_API_KEY ? "resend" : "smtp"));
     // eslint-disable-next-line no-console
     console.log(`Server running on port ${PORT}`);
+    // eslint-disable-next-line no-console
+    console.log(`Email provider mode: ${emailProvider}`);
   });
 };
 
 startServer();
-
