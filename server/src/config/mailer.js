ea19e945155cc0baaff06adab688d733240c1b4f
Subhadip Mondal <subhadipmondal0101@gmail.com>
2026-03-02 11:20:20 +0530

    Add timeout settings to mailer transport configuration

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -10,6 +10,9 @@ const getMailerTransporter = async () => {
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT || 587),
       secure: String(process.env.SMTP_SECURE || "false") === "true",
+      connectionTimeout: Number(process.env.SMTP_CONNECTION_TIMEOUT_MS || 10000),
+      greetingTimeout: Number(process.env.SMTP_GREETING_TIMEOUT_MS || 10000),
+      socketTimeout: Number(process.env.SMTP_SOCKET_TIMEOUT_MS || 20000),
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
@@ -25,4 +28,3 @@ const getMailerTransporter = async () => {
 };
 
 module.exports = { getMailerTransporter };
-
