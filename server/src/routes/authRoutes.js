b86386da9c46100ecffb441cf90948cd070a44d9
Subhadip Mondal <subhadipmondal0101@gmail.com>
2026-03-02 11:22:35 +0530

    Update authRoutes.js

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const { register, verifyEmail, login, getMe } = require("../controllers/authController");
+const { register, verifyEmail, login, resendVerification, getMe } = require("../controllers/authController");
 const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/verify-email", verifyEmail);
+router.post("/resend-verification", resendVerification);
 router.post("/login", login);
 router.get("/me", protect, getMe);
 
 module.exports = router;
-
